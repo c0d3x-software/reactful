5a14f14767e9a7a2b1159be75a2f4210df1b212f
jsenaribeiro <jsenaribeiro@gmail.com>
2025-06-09 00:05:05 -0300

    tests: fn-decorator

--- packages/server/loader/fn-decorator/coder.test.ts
+++ packages/server/loader/fn-decorator/coder.test.ts
@@ -2,54 +2,6 @@ import { expect, test } from "bun:test";
 import { coder } from "./coder";
 
 const oldCode = `
-function log(meta: ImportMeta, target: Function) {
-   console.log("Decorator called!", meta.url, target.name);
-   return function (param1: any, param2: any) {
-      return target;
-   };
-}
-
-@log('argA', 123)
-function myFunction(value: string) {
-   console.log("...");
-}
-
-@log('hello')
-function anotherFunction() {
-   console.log('anotherFunction called!');
-}
-
-@log(3)
-const arrowFunction = () => 1+2
-`
-
-const newCode = `
-function log(meta: ImportMeta, target: Function) {
-   console.log("Decorator called!", meta.url, target.name);
-   return function (param1: any, param2: any) {
-      return target;
-   };
-}
-
-function myFunction(value: string) {
-   console.log("...");
-}
-
-function anotherFunction() {
-   console.log('anotherFunction called!');
-}
-
-const arrowFunction = () => 1+2
-log(import.meta, arrowFunction)(3);
-log(import.meta, anotherFunction)('hello');
-log(import.meta, myFunction)('argA', 123);`
-
-// test('fn-decorator: success', function () {
-//    const code = coder(oldCode)?.trim()
-//    expect(code).toBe(newCode.trim())
-// })
-
-const tmpCode = `
 @log(true) function myFunction() {}
 @log(true) async function myAsyncFunction() {}
 @log(true) function* myGenFunction() { yield 1; }
@@ -70,9 +22,73 @@ const tmpCode = `
 @log(true) export default function namedExportedFunction() {}`
 
 
-test('fn-decorator', function () {
-   const info = { count:0 }
-   const code = coder(tmpCode, info)?.trim()
-   console.log(code)
-   console.log(tmpCode.trim().split('\n').length, info.count)
+const newCode = `
+function myFunction() {}
+async function myAsyncFunction() {}
+function* myGenFunction() { yield 1; }
+async function* myAsyncGenFunction() { yield 1; }
+export function exportedFunction() {}
+export async function exportedAsyncFunction() {}
+export default function namedDefault() {}
+const funcExpr = function namedFuncExpr() {};
+const asyncFuncExpr = async function namedAsyncFuncExpr() {};
+const genFuncExpr = function* namedGenFuncExpr() { yield 1; };
+const asyncGenFuncExpr = async function* namedAsyncGenFuncExpr() { yield 1; };
+const arrow = () => {};
+const asyncArrow = async () => {};
+const implicitArrow = (a, b) => a + b;
+const asyncImplicitArrow = async () => await Promise.resolve("done");
+export default function namedDefaultExport() {}
+export default async function namedAsyncDefaultExport() {}
+export default function namedExportedFunction() {}
+
+log(import.meta, namedExportedFunction)(true);
+log(import.meta, namedAsyncDefaultExport)(true);
+log(import.meta, namedDefaultExport)(true);
+log(import.meta, asyncImplicitArrow)(true);
+log(import.meta, implicitArrow)(true);
+log(import.meta, asyncArrow)(true);
+log(import.meta, arrow)(true);
+log(import.meta, asyncGenFuncExpr)(true);
+log(import.meta, genFuncExpr)(true);
+log(import.meta, asyncFuncExpr)(true);
+log(import.meta, funcExpr)(true);
+log(import.meta, namedDefault)(true);
+log(import.meta, exportedAsyncFunction)(true);
+log(import.meta, exportedFunction)(true);
+log(import.meta, myAsyncGenFunction)(true);
+log(import.meta, myGenFunction)(true);
+log(import.meta, myAsyncFunction)(true);
+log(import.meta, myFunction)(true);`
+
+const cases = [
+   'namedExportedFunction',
+   'namedAsyncDefaultExport',
+   'namedDefaultExport',
+   'asyncImplicitArrow',
+   'implicitArrow',
+   'asyncArrow',
+   'arrow',
+   'asyncGenFuncExpr',
+   'genFuncExpr',
+   'asyncFuncExpr',
+   'funcExpr',
+   'namedDefault',
+   'exportedAsyncFunction',
+   'exportedFunction',
+   'myAsyncGenFunction',
+   'myGenFunction',
+   'myAsyncFunction',
+   'myFunction']
+
+test(`fn-decorator: success`, function () {
+   const code = coder(oldCode)?.trim()
+   expect(code.trim()).toBe(newCode.trim())
+})   
+
+cases.forEach(function (name) {
+   test(`fn-decorator: ${name} success`, function () {
+      const code = coder(oldCode)?.trim()
+      expect(code.trim()).toContain(`log(import.meta, ${name})(true)`)
+   })   
 })
